b5317dbdfd502b7e77b749c01a864e521f20f5f7
Efosa Lawal <fodrive@FoMacProHS.local>
2023-11-07 04:19:46 +0000

    1st Customization of Routes and Controllers in API

--- src/api/job/controllers/job.js
+++ src/api/job/controllers/job.js
@@ -1,9 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  * job controller
  */
 
+/*
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::job.job');
+*/
+
+module.exports = ({ strapi }) => ({
+  async find(ctx) {
+    // console.log("ctx", ctx);
+    try {
+      ctx.body = "Hello World!";
+    } catch (error) {
+      strapi.log.error(error);
+    }
+  },
+});
--- src/api/job/routes/job.js
+++ src/api/job/routes/job.js
@@ -1,9 +1,22 @@
-'use strict';
+// "use strict";
 
 /**
  * job router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+/*
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::job.job');
+module.exports = createCoreRouter("api::job.job");
+*/
+
+module.exports = {
+  routes: [
+    {
+      // Path defined with an URL parameter
+      method: "GET",
+      path: "/jobs",
+      handler: "job.find",
+    },
+  ],
+};
