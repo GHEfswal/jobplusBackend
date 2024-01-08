9bc25f31e18a553daa44002f732bdea58ed05baa
Efosa Lawal <fodrive@FoMacProHS.local>
2024-01-08 22:07:34 +0000

    Applied Job CRUD

--- src/api/applied-job/controllers/applied-job.js
+++ src/api/applied-job/controllers/applied-job.js
@@ -1,9 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  * applied-job controller
  */
-
+/*
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::applied-job.applied-job');
+*/
+
+module.exports = ({ strapi }) => ({
+  async apply(ctx) {
+    try {
+      ctx.body = await strapi
+        .service("api::applied-job")
+        .apply(ctx.request.body);
+    } catch (error) {
+      strapi.log.error(error);
+    }
+  },
+  async withdrawApplication(ctx) {
+    try {
+      const { jobId, userId } = ctx.params;
+      ctx.body = await strapi
+        .service("api::applied-job")
+        .withdrawApplication(jobId, userId);
+    } catch (error) {
+      strapi.log.error(error);
+    }
+  },
+});
--- src/api/applied-job/services/applied-job.js
+++ src/api/applied-job/services/applied-job.js
@@ -1,9 +1,64 @@
-'use strict';
+"use strict";
 
 /**
  * applied-job service
  */
-
+/*
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::applied-job.applied-job');
+*/
+module.exports = ({ strapi }) => ({
+  async apply(data) {
+    try {
+      const entity = await strapi.entityService.create(
+        "api::applied-job.applied-job",
+        {
+          data: data,
+          populate: {
+            user: true,
+            job: true,
+          },
+        }
+      );
+      return entity;
+    } catch (error) {
+      strapi.log.error(`Error: ${error.message}`, {
+        data,
+      });
+      throw error;
+    }
+  },
+  async withdrawApplication(jobId, userId) {
+    try {
+      const appliedJobs = await strapi.entityService.findMany(
+        "api::applied-job.applied-job",
+        {
+          filters: {
+            job: jobId,
+            user: userId,
+          },
+          populate: {
+            user: {
+              fields: ["id", "username", "email"],
+            },
+            job: {
+              fields: ["id", "title"],
+            },
+          },
+        }
+      );
+
+      // delete applied job
+      appliedJobs.map((job) =>
+        strapi.entityService.delete("api::applied-job.applied-job", job.id)
+      );
+
+      // return deleted applied job
+      return appliedJobs;
+    } catch (error) {
+      strapi.log.error(`Error: ${error.message}`);
+      throw error;
+    }
+  },
+});
